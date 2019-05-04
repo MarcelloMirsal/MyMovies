7d59339d673893847c8c908fdbb631774f6f081f
Marcello Mirsal <marcellomirsal@marcellos-macbook-pro.local>
2019-05-04 21:08:53 +0300

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Marcello Mirsal on 04/05/2019.
+//  Copyright © 2019 Marcello Mirsal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
